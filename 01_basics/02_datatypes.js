513f329db61256c5615eee42ca255fdd18b8a74d
Rahul Ambiger <139382710+RahulAmbiger11@users.noreply.github.com>
2024-04-19 05:03:18 +0000

    datatypes in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,45 @@
+//Datatypes in Js
+
+"use strict" // treat all js code as newer version
+// alert("hello")
+// alert(3+3) //not possible in node.js not browser
+
+//Code Readability is Recommended
+console.log("Ramcharan")
+
+let name = "Rahul"
+let age = 18
+let isLoggedIn = true
+let state; //undefined - value not assigned
+let state1 = null //null
+
+//Primitive Datatypes:
+//number → 2 to the power 53
+//bigint
+//string → ""
+//boolean → true/false
+//null - standalone value
+//undefined
+//symbol - unique 
+
+
+//Object
+console.log(typeof "rahul") //string
+console.log(typeof age) // number
+
+// ** IMP IQ **
+console.log(typeof state) //undefined
+console.log(typeof state1) //object
+
+
+
+
+
+
+
+
+
+//EcmaScript Official Documentation: https://tc39.es/ecma262/
+//mdn Documentation: https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+
+
