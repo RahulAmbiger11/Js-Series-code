ba43cc93fda44cbc187fed4d9d6f9e167c4233bf
Rahul Ambiger <139382710+RahulAmbiger11@users.noreply.github.com>
2024-04-19 05:58:46 +0000

    Comparison operations in js

---
+++ 01_basics/04_comprison.js
@@ -0,0 +1,37 @@
+
+//Comparison operations in js
+// console.log(2>1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+//No recommended 
+console.log("2">1) //true
+console.log("02" > 1) //true
+
+//when we are comparing 2 values it's datatypes should be same
+
+//IMP IQ
+//NOT RECOMMENDED IN  - Development codebase
+console.log(null > 0) //false
+console.log(null == 0) //false
+console.log(null >= 0) //true
+
+//Note: 
+// Equality operator & Comparison operators work differently.
+// Comparison operators (<=, >= ) convert null → 0
+//O/P - true
+// Equality operator(===) - doesn't convert it to 0
+//O/P - false
+
+console.log(undefined == 0) //false
+console.log(undefined > 0) //false
+console.log(undefined < 0) //false
+console.log(undefined <= 0) //false
+console.log(undefined >= 0) //false
+
+//strict check ===
+// false
+console.log("2" === 2) // value & datatype check
