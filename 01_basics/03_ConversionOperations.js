68b25ed8eb1c4348674d5204eab454e77128b43c
Rahul Ambiger <139382710+RahulAmbiger11@users.noreply.github.com>
2024-04-19 05:43:20 +0000

    Conversion Operations in Js

--- 01_basics/03_ConversionOperations.js
+++ 01_basics/03_ConversionOperations.js
@@ -32,3 +32,49 @@ let SomeNum = 33
 let strNum = String(SomeNum)
 console.log(strNum) //33
 console.log(typeof strNum) //string
+
+//=================================================================
+
+//Operations
+
+let value = 3
+let negValue = -value
+console.log(negValue) //-3
+
+//Basic Math Operations
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str2 = "hello"
+let str3 = " rahul"
+let str4 = str2 + str3
+console.log(str4)  
+
+//datatype of first value is preffered
+console.log("1" + 2) //12
+console.log(1 + "2") //12
+console.log("1" + "2") //12
+console.log("1" + 2 + 2) //122
+//IMP - here 1st values are in number format, based on that respective operation is performed
+console.log(2+2+"1") //41
+
+console.log((3+4) * 5 % 3) //2
+
+//Refer for Abstract Operations: https://tc39.es/ecma262/#sec-algorithm-conventions-abstract-operations
+
+
+//Not Recommended
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+
+//prefix and postfix: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+let gameCounter = 100
+++gameCounter; //prefix
+console.log(gameCounter)
+
+gameCounter++; //postfix
+console.log(gameCounter)
