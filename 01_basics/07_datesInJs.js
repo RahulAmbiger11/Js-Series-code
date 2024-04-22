c484ee12d86aaa9592133e151d3519edc34e7e39
Rahul Ambiger <139382710+RahulAmbiger11@users.noreply.github.com>
2024-04-22 01:46:06 +0000

    Dates in Js

--- 01_basics/07_datesInJs.js
+++ 01_basics/07_datesInJs.js
@@ -13,5 +13,55 @@ console.log(myDate.toLocaleString())
 console.log(typeof myDate) //object
 
 //months in js strats from - 0
-let createdDate = new Date(2024, 0, 24)
-console.log(createdDate.toDateString()) 
+let createdDate = new Date(2023, 0, 24, 5, 4)
+console.log(createdDate.toDateString()) //Tue Jan 24 2023
+console.log(createdDate.toLocaleString()) //1/24/2023, 5:04:00 AM
+
+//specific format
+
+//foreign fomat of date
+let myCreatedDate = new Date("2024-01-23")
+console.log(myCreatedDate.toLocaleString())
+
+//indian format
+let myCreatedOne = new Date("03-12-2023") //mm-dd-yyyy
+console.log(myCreatedOne.toLocaleString())
+
+
+let myDT = new Date("3-23-2024") //mm-dd-yyyy
+console.log(myDT.toLocaleString()) 
+
+//Timestamps
+let myTStamp = Date.now()
+console.log(myTStamp) //millisec's
+
+//easy for comparison
+console.log(myDT.getTime()) //millisec's
+
+
+//** IMP IQ **
+//millisec's → sec's
+console.log(Math.floor(Date.now()/1000)) //sec's
+
+//or
+console.log(Math.round(Date.now()/1000)) //sec's
+
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth() + 1) //starts from 0 ie, Apr - 3
+console.log(newDate.getDay()) //mon -1
+
+
+//** IMP **
+//toLocaleString()
+
+let newOne = newDate.toLocaleString('default', {
+    weekday: "long"
+})
+
+console.log(newOne)
+
+
+
+
