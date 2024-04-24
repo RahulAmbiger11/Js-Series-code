9c62a17768fffe9e0366f642498d428d23fe631b
Rahul Ambiger <139382710+RahulAmbiger11@users.noreply.github.com>
2024-04-24 04:08:03 +0000

    scopes in js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -26,3 +26,57 @@ console.log(c) //500
 Scope of Windows Browser js environment &
 Scope of Coding environment using Node.js are Different.
  */
+
+//Nested Scopes
+
+//we can access the parent function variables inside child function
+function one() {
+    let username = "rahul"
+
+    function two() {
+        let website = "youtube"
+        console.log(username)
+    }
+    // console.log(website)
+    two()
+}
+one()
+
+
+if (true) {
+    const username = "rahul"
+    if(username ===  "rahul") {
+        const website = " youtube"
+        console.log(username + website)
+    }
+    // console.log(website)
+}
+// console.log(username)
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++
+// ** IMP IQ **
+// Ways to create functions
+
+//1. Function as  a declaration
+
+console.log(addOne(5))
+
+function addOne(num) {
+    return num+1
+}
+
+
+//2. function as a expression
+// console.log(addTwo())
+
+const addTwo = function(num) {
+    return num+2
+}
+
+/*
+(**IMP IQ **)
+Variable Hoisting:
+When we declare a function as an expression, and if you try to invoke that particular function before the function, then it is going to throw the error. It is because of "variable hoisting".
+
+- It depends on function scope and how js treats variables etc.
+*/
