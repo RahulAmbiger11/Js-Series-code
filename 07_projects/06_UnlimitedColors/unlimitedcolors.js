d0505c84c7d837d7305f5435647132ff3b3a2ec2
Rahul Ambiger <139382710+RahulAmbiger11@users.noreply.github.com>
2024-05-05 07:10:50 +0000

    P-6 unlimitedColors

---
+++ 07_projects/06_UnlimitedColors/unlimitedcolors.js
@@ -0,0 +1,30 @@
+//generate random colors
+
+let randomColor = () => {
+    let hex = "0123456789ABCDEF"
+    let color = "#"
+
+    for(let i = 0; i < 6; i++) {
+       color += hex[Math.floor( Math.random()*16)]
+    }
+    return color
+}
+
+let intervalId
+
+//start
+document.querySelector('#start').addEventListener('click', ()=>{
+    //handling edge cases
+    if(!intervalId) {
+        intervalId = setInterval(()=>{
+            document.body.style.backgroundColor = randomColor()
+        }, 1000)
+    }
+})
+
+//stop
+document.querySelector('#stop').addEventListener('click',()=>{
+    clearInterval(intervalId)
+    //handling edge cases
+    intervalId = null
+})
