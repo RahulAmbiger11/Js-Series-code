8bf5f4603a7665f15ee8a0582c6e5d8cb31b2aa8
Rahul Ambiger <139382710+RahulAmbiger11@users.noreply.github.com>
2024-06-06 07:38:09 +0000

    oops in js

--- 10_classes_and_oop_in_Js/oop.js
+++ 10_classes_and_oop_in_Js/oop.js
@@ -7,10 +7,64 @@ const user = {
 
     //methods
     getuserDetails:function() {
-        console.log("Got user details from database")
+        // console.log("Got user details from database")
+        //this - refers to current context
+        // console.log(`Username: ${this.username}`)
+
+        //functional context
+        console.log(this);
     }
 }
 
 console.log(user.username)
-console.log(user.getuserDetails())
+// console.log(user.getuserDetails())
+
+//global context
+console.log(this) //{}
+
+
+//Constructor function
+//refer: https://youtu.be/_TjtAyMkiTI?t=25191
+
+/* const promiseOne = new Promise()
+const date = new Date() */
+
+
+class User {
+    constructor(username, loginCount, isLoggedIn) {
+        this.username = username;
+        this.loginCount = loginCount;
+        this.isLoggedIn = isLoggedIn;
+
+        this.geeeting = function () {
+            console.log(`Welcome ${this.username}`);
+        };
+
+        //defined implicitly
+        // return this
+    }
+}
+
+//creating diff. instances using "new" keyword
+const userOne = new User("rahul",12,true)
+const userTwo = new User("ram",11,true)
+console.log(userOne.constrctor)
+// console.log(userTwo)
+
+
+//instanceOf() in js - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/instanceof
+
+//Example of instanceOf()
+class Car {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+}
+
+const auto = new Car("Honda","Accord",1998)
+const autoTwo = new Car("Honda","Amaze",2002)
+console.log(auto instanceof Car) //true
+console.log(autoTwo instanceof Car) //true
 
